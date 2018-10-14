5314a8ccd2aeffce05217de739d1f5f761602c3c
Yaseen Davids <yaseendavids477@gmail.com>
2018-10-14 12:16:20 +0200

    all id changed to class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $("#home-section-title").css('transform', 'scale(1)');
+    $(".home-section-title").css('transform', 'scale(1)');
 
     const skillsPos = $("#skills_section").position().top - 50;
     const expPos = $("#exp_section").position().top - 50;
@@ -13,42 +13,42 @@ $(document).ready(function () {
         var scroll = $(window).scrollTop();
 
         if (window.screen.width < 500) {
-            $("#navbar").css('height', '60px');
-            $("#navbar").css('background', 'transparent');
-            $("#navbar_links a").css('display', 'none');
+            $(".navbar").css('height', '60px');
+            $(".navbar").css('background', 'transparent');
+            $(".navbar_links a").css('display', 'none');
         }
 
         if (scroll > 100) {
-            $("#navbar").css('background', 'rgb(23, 25, 29)');
+            $(".navbar").css('background', 'rgb(23, 25, 29)');
         }
 
         if (scroll < 100) {
-            $("#navbar").css('background', 'transparent');
+            $(".navbar").css('background', 'transparent');
         }
 
         if (scroll < skillsPos) {
-            $("#navbar_links a").removeClass('position');
+            $(".navbar_links a").removeClass('position');
         }
 
         if (scroll > skillsPos) {
-            $("#navbar_links a").removeClass('position');
-            $("#navbar_links a:nth-child(2)").first().addClass('position');
+            $(".navbar_links a").removeClass('position');
+            $(".navbar_links a:nth-child(2)").first().addClass('position');
         }
         if (scroll > expPos) {
-            $("#navbar_links a").removeClass('position');
-            $("#navbar_links a:nth-child(3)").first().addClass('position');
+            $(".navbar_links a").removeClass('position');
+            $(".navbar_links a:nth-child(3)").first().addClass('position');
         }
         if (scroll > educPos) {
-            $("#navbar_links a").removeClass('position');
-            $("#navbar_links a:nth-child(4)").first().addClass('position');
+            $(".navbar_links a").removeClass('position');
+            $(".navbar_links a:nth-child(4)").first().addClass('position');
         }
         if (scroll > projPos) {
-            $("#navbar_links a").removeClass('position');
-            $("#navbar_links a:nth-child(5)").first().addClass('position');
+            $(".navbar_links a").removeClass('position');
+            $(".navbar_links a:nth-child(5)").first().addClass('position');
         }
         if (scroll > aboutPos) {
-            $("#navbar_links a").removeClass('position');
-            $("#navbar_links a:nth-child(6)").first().addClass('position');
+            $(".navbar_links a").removeClass('position');
+            $(".navbar_links a:nth-child(6)").first().addClass('position');
         }
     });
 
@@ -56,34 +56,22 @@ $(document).ready(function () {
         $('html,body').animate({ scrollTop: skillsPos + 20 }, 1000);
     })
 
-    $("#navbar_links a").on('click', function () {
-        if ($(this).html() == "Skills") {
-            $('html,body').animate({ scrollTop: skillsPos + 20 }, 1000);
-        }
-        if ($(this).html() == 'Experience') {
-            $('html,body').animate({ scrollTop: expPos + 20 }, 1000);
-        }
-        if ($(this).html() == 'Education') {
-            $('html,body').animate({ scrollTop: educPos + 20 }, 1000);
-        }
-        if ($(this).html() == 'Projects') {
-            $('html,body').animate({ scrollTop: projPos + 20 }, 1000);
-        }
-        if ($(this).html() == 'About') {
-            $('html,body').animate({ scrollTop: aboutPos + 20 }, 1000);
-        }
+    $(".navbar_links a").on('click', function () {
+        $('html,body').animate({ 
+            scrollTop: $($.attr(this, 'href')).offset().top - 100
+        }, 1000);
     })
 
-    $("#hamburger_menu").click(function () {
-        if ($("#navbar_links a").css('display') == 'none') {
-            $("#navbar").css('height', '400px');
-            $("#navbar").css('background', 'rgb(23, 25, 29)');
-            $("#navbar_links a").css('display', 'block');
+    $(".hamburger_menu").click(function () {
+        if ($(".navbar_links a").css('display') == 'none') {
+            $(".navbar").css('height', '400px');
+            $(".navbar").css('background', 'rgb(23, 25, 29)');
+            $(".navbar_links a").css('display', 'block');
         }
         else {
-            $("#navbar").css('height', '60px');
-            $("#navbar").css('background', 'transparent');
-            $("#navbar_links a").css('display', 'none');
+            $(".navbar").css('height', '60px');
+            $(".navbar").css('background', 'transparent');
+            $(".navbar_links a").css('display', 'none');
         }
 
     })
